ca2722a2d00a109ac25bfd7a6314d81bdf3e107c
asumpta <sumgathoni@gmail.com>
2023-01-19 13:29:48 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Add your code here
+// function submitData(name, email) {
+//     const data = { name, email };
+//     const options = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//       },
+//       body: JSON.stringify(data)
+//     };
+  
+//     fetch('http://localhost:3000/users', options)
+//       .then(response => response.json())
+//       .then(data => console.log(data))
+//       .catch(error => console.error('Error:', error));
+//   }
+
+  //
+
+  function submitData(name, email) {
+    const data = { name, email };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(data)
+    };
+  
+    fetch('http://localhost:3000/users', options)
+      .then(response => response.json())
+      .then(data => {
+        const newId = data._id;
+        const idContainer = document.getElementById("newId");
+        idContainer.innerText = newId;
+      })
+      .catch(error => {
+        const errorContainer = document.getElementById("error");
+        errorContainer.innerText = error.message;
+      });
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
